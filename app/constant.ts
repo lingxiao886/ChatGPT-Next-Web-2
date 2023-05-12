15d70ffe6ea53c7316b0523b50948ab6e77fbdcb
lingxiao886 <129174357+lingxiao886@users.noreply.github.com>
2023-05-12 17:37:30 +0800

    Update constant.ts
    是

--- app/constant.ts
+++ app/constant.ts
@@ -1,12 +1,11 @@
-export const OWNER = "liruqi";
+export const OWNER = "Yidadaa";
 export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
-export const UPDATE_URL = `${REPO_URL}#keep-updated`;
+export const REPO_URL = `https://mp.weixin.qq.com/s/o6mOTqdocVLs7AORHAhlEw`;
+export const ISSUE_URL = `https://mp.weixin.qq.com/s/o6mOTqdocVLs7AORHAhlEw`;
+export const UPDATE_URL = `https://mp.weixin.qq.com/s/o6mOTqdocVLs7AORHAhlEw`
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
-
 export enum Path {
   Home = "/",
   Chat = "/chat",
